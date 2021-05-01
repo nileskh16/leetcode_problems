f287612988d643ef3f5377ad631e4ab8cea83d94
Nilesh Swami <Nilesh.Swami@nice.com>
2021-05-01 11:39:44 +0530

    PowerInteger - 30th April, 2021

---
+++ src/main/java/com/plural/spring/fundamentals/leetcodes/PowerIntegers.java
@@ -0,0 +1,41 @@
+package com.plural.spring.fundamentals.leetcodes;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PowerIntegers {
+
+    public static void main(String[] args) {
+        for (Integer powerNumber : new PowerIntegers().getPowerIntegers(2, 3, 10)) {
+            System.out.println(powerNumber);
+        }
+    }
+
+    public List<Integer> getPowerIntegers(int x, int y, int bound) {
+        Set<Integer> targetNumbers = new HashSet<>();
+        int i = getHighestPower(bound, x);
+        int j = getHighestPower(bound, y);
+        if (bound > 0) {
+            for (int row=0; row<=i; row++) {
+                for (int col=0; col<=j; col++) {
+                    double num = Math.pow(x, row) + Math.pow(y, col);
+                    if (bound >= num) targetNumbers.add((int) num);
+                }
+            }
+        }
+        return new ArrayList<>(targetNumbers);
+    }
+
+    private int getHighestPower(int bound, int source) {
+        if (bound == 0) return 0;
+        if (source <= 1) return source;
+        int count = 0;
+        while (bound >= Math.pow(source, count)) {
+            count++;
+        }
+        return count - 1;
+    }
+}
---
+++ src/test/java/leetcode/PowerIntegerTest.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import com.plural.spring.fundamentals.leetcodes.PowerIntegers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class PowerIntegerTest {
+    private final PowerIntegers powerIntegers = new PowerIntegers();
+
+    @Test
+    public void testOne() {
+        List<Integer> numbers = powerIntegers.getPowerIntegers(3, 2, 10);
+        Collections.sort(numbers);
+        assertThat(Arrays.asList(2, 3, 4, 5, 7, 9, 10), is(numbers));
+    }
+
+    @Test
+    public void testTwo() {
+        List<Integer> numbers = powerIntegers.getPowerIntegers(3, 5, 15);
+        Collections.sort(numbers);
+        assertThat(Arrays.asList(2, 4, 6, 8, 10, 14), is(numbers));
+    }
+}
